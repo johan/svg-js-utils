eaaa35d075e39e8d37ac1cefd0b7c612e0d7252d
Johan Sundström <oyasumi@gmail.com>
2012-07-07 20:46:29 -0700

    More readable code.

--- absolutizePath.js
+++ absolutizePath.js
@@ -1,33 +1,54 @@
 // Turns all path commands into absolute coordinates, in-place. Origin: Phrogz,
 // http://stackoverflow.com/questions/9677885/convert-svg-path-to-absolute-commands
 function absolutizePath(path) {
-  var x0,y0,x1,y1,x2,y2,segs = path.pathSegList;
-  for (var x=0,y=0,i=0,len=segs.numberOfItems;i<len;++i){
-    var seg = segs.getItem(i), c=seg.pathSegTypeAsLetter;
-    if (/[MLHVCSQTA]/.test(c)){
-      if ('x' in seg) x=seg.x;
-      if ('y' in seg) y=seg.y;
-    }else{
-      if ('x1' in seg) x1=x+seg.x1;
-      if ('x2' in seg) x2=x+seg.x2;
-      if ('y1' in seg) y1=y+seg.y1;
-      if ('y2' in seg) y2=y+seg.y2;
-      if ('x'  in seg) x+=seg.x;
-      if ('y'  in seg) y+=seg.y;
-      switch(c){
-        case 'm': segs.replaceItem(path.createSVGPathSegMovetoAbs(x,y),i);                   break;
-        case 'l': segs.replaceItem(path.createSVGPathSegLinetoAbs(x,y),i);                   break;
-        case 'h': segs.replaceItem(path.createSVGPathSegLinetoHorizontalAbs(x),i);           break;
-        case 'v': segs.replaceItem(path.createSVGPathSegLinetoVerticalAbs(y),i);             break;
-        case 'c': segs.replaceItem(path.createSVGPathSegCurvetoCubicAbs(x,y,x1,y1,x2,y2),i); break;
-        case 's': segs.replaceItem(path.createSVGPathSegCurvetoCubicSmoothAbs(x,y,x2,y2),i); break;
-        case 'q': segs.replaceItem(path.createSVGPathSegCurvetoQuadraticAbs(x,y,x1,y1),i);   break;
-        case 't': segs.replaceItem(path.createSVGPathSegCurvetoQuadraticSmoothAbs(x,y),i);   break;
-        case 'a': segs.replaceItem(path.createSVGPathSegArcAbs(x,y,seg.r1,seg.r2,seg.angle,seg.largeArcFlag,seg.sweepFlag),i);   break;
-        case 'z': case 'Z': x=x0; y=y0; break;
+  var segs = path.pathSegList
+    , len  = segs.numberOfItems
+    , M    = 'createSVGPathSegMovetoAbs'
+    , L    = 'createSVGPathSegLinetoAbs'
+    , H    = 'createSVGPathSegLinetoHorizontalAbs'
+    , V    = 'createSVGPathSegLinetoVerticalAbs'
+    , C    = 'createSVGPathSegCurvetoCubicAbs'
+    , S    = 'createSVGPathSegCurvetoCubicSmoothAbs'
+    , Q    = 'createSVGPathSegCurvetoQuadraticAbs'
+    , T    = 'createSVGPathSegCurvetoQuadraticSmoothAbs'
+    , A    = 'createSVGPathSegArcAbs'
+    , seg, abs, x, y, i, c, x0, y0, x1, y1, x2, y2;
+  for (x = y = i = 0; i < len; i++) {
+    seg = segs.getItem(i);
+    c   = seg.pathSegTypeAsLetter;
+    abs = undefined;
+    if (/[MLHVCSQTA]/.test(c)) {
+      // already absolute; just update current coord
+      if ('x' in seg) x = seg.x;
+      if ('y' in seg) y = seg.y;
+    }
+    else {
+      if ('x1' in seg) x1 = x + seg.x1;
+      if ('x2' in seg) x2 = x + seg.x2;
+      if ('y1' in seg) y1 = y + seg.y1;
+      if ('y2' in seg) y2 = y + seg.y2;
+      if ('x'  in seg) x += seg.x;
+      if ('y'  in seg) y += seg.y;
+      switch (c) {
+        case 'm': abs = path[M](x, y); break;
+        case 'l': abs = path[L](x, y); break;
+        case 'h': abs = path[H](x); break;
+        case 'v': abs = path[V](y); break;
+        case 'c': abs = path[C](x, y, x1, y1, x2, y2); break;
+        case 's': abs = path[S](x, y, x2, y2); break;
+        case 'q': abs = path[Q](x, y, x1, y1); break;
+        case 't': abs = path[T](x, y); break;
+        case 'a': abs = path[A](x, y, seg.r1, seg.r2, seg.angle,
+                                seg.largeArcFlag, seg.sweepFlag); break;
+        case 'z':
+        case 'Z': x = x0; y = y0; break;
       }
+      if (abs) segs.replaceItem(abs, i);
     }
     // Record the start of a subpath
-    if (c=='M' || c=='m') x0=x, y0=y;
+    if (c == 'M' || c == 'm') {
+      x0 = x;
+      y0 = y;
+    }
   }
 }
